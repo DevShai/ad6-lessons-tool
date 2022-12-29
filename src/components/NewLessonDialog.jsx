0679f5bdfff1ff8742112d595987425542ced67c
ben <ben.egg.gov@gmail.com>
2022-12-29 15:41:02 +0200

    update new lesson dialog

--- src/components/NewLessonDialog.jsx
+++ src/components/NewLessonDialog.jsx
@@ -1,79 +1,96 @@
 import React, { useState } from 'react';
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
+import '../styles/NewLessonDialog.css'
 
-const form = (
-    <Form>
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-                שם השיעור
-            </Form.Label>
-            <Col sm={10}>
-                <Form.Control type="text" placeholder="" />
-            </Col>
-        </Form.Group>
 
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-                צליל השיעור
-            </Form.Label>
-            <Col sm={10}>
-                <Form.Select>
-                    <option value="mixed">מעורב</option>
-                    <option value="a">קמץ, פתח</option>
-                    <option value="e">חיריק</option>
-                    <option value="i">חולם</option>
-                    <option value="o">קובוץ, שורוק</option>
-                    <option value="u">סגול, צירה</option>
-                </Form.Select>
-            </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-                אותיות
-            </Form.Label>
-            <Col sm={10}>
-                <Form.Control type="text" placeholder="" />
-            </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-                מבחן
-            </Form.Label>
-            <Col sm={10}>
-                <Form.Check
-                    type="switch"
-                    id="is_test"
-                />
-            </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-                ערבוב שאלות
-            </Form.Label>
-            <Col sm={10}>
-                <Form.Check
-                    type="switch"
-                    id="is_shuffle"
-                />
-            </Col>
-        </Form.Group>
-        <center>
-        <Button type='submit'>שמירה</Button>
-        </center>
-    </Form>
-)
 
 export default function NewLessonDialog(props) {
+
+    const [lessonData, setLessonData] = useState({
+        name: '',
+        sound: 'mixed',
+        letters: [],
+        exam: false,
+        shuffle: false,
+    })
+
+    const form = (
+        <Form>
+            <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm={2}>
+                    שם השיעור
+                </Form.Label>
+                <Col sm={10}>
+                    <Form.Control 
+                    required 
+                    type="text" 
+                    placeholder="" 
+                    onChange={(e) => setLessonData({...lessonData, name: e.target.value})}/>
+                </Col>
+            </Form.Group>
+    
+            <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm={2}>
+                    צליל השיעור
+                </Form.Label>
+                <Col sm={10}>
+                    <Form.Select onChange={(e) => setLessonData({...lessonData, sound: e.target.value})}>
+                        <option value="mixed">מעורב</option>
+                        <option value="a">קמץ, פתח</option>
+                        <option value="e">חיריק</option>
+                        <option value="i">חולם</option>
+                        <option value="o">קובוץ, שורוק</option>
+                        <option value="u">סגול, צירה</option>
+                    </Form.Select>
+                </Col>
+            </Form.Group>
+    
+            <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm={2}>
+                    אותיות
+                </Form.Label>
+                <Col sm={10}>
+                    <Form.Control type="text" placeholder="" />
+                </Col>
+            </Form.Group>
+    
+            <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm={2}>
+                    מבחן
+                </Form.Label>
+                <Col sm={10} className="switch-container">
+                    <Form.Check
+                        type="switch"
+                        id="is_test"
+                        onChange={(e) => setLessonData({...lessonData, exam: e.target.checked})}
+                    />
+                </Col>
+            </Form.Group>
+    
+            <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm={2}>
+                    ערבוב שאלות
+                </Form.Label>
+                <Col sm={10} className="switch-container">
+                    <Form.Check
+                        type="switch"
+                        id="is_shuffle"
+                        onChange={(e) => setLessonData({...lessonData, shuffle: e.target.checked})}
+                    />
+                </Col>
+            </Form.Group>
+            <center>
+            <Button type='submit'>שמירה</Button>
+            </center>
+        </Form>
+    )
+
     return (
         <>
             <Modal
                 show={props.visible}
                 onHide={props.onHide}
-                backdrop="static"
                 keyboard={false}
                 size='lg'
             >
